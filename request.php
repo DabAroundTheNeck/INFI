ea27f5af6c0b966e280ffaa9a62d8b2a9a5d4c1c
Michi2806 <michi2806@gmail.com>
2017-11-22 12:56:05 +0100

    Data

--- request.php
+++ request.php
@@ -36,7 +36,7 @@
     if (mysqli_num_rows($result) > 0) {
         // output data of each row
         while($row = mysqli_fetch_assoc($result)) {
-            echo "id: " . $row["id"]. " - Name: " . $row["f_name"]. " " . $row["l_name"]. . $row["short"] . "<br>";
+            echo "id: " . $row["id"]. " - Name: " . $row["f_name"]. " " . $row["l_name"]. " Kürzl " . $row["short"] . "<br>";
         }
 
     } else {
