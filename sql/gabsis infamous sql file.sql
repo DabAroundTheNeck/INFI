8ded242bb521b3366b806bcc7a9850d43f6f2658
Gabsii <lukas.gabsi@tele2.at>
2017-12-13 14:07:16 +0100

    changes
    changed

--- sql/gabsis infamous sql file.sql
+++ sql/gabsis infamous sql file.sql
@@ -76,7 +76,7 @@ ENGINE = InnoDB;
 INSERT INTO `teachers` (`id`, `f_name`, `l_name`, `short`) VALUES (NULL, 'Katzengruber', 'Leo', 'leka'), (NULL, 'Blank', 'Michael', 'mibl'), (NULL, 'Gabsi', 'Lukas', 'luga');
 INSERT INTO `subjects` (`id`, `lvnr`, `title`, `groups_required`) VALUES (NULL, '100-3342', 'Betriebstechnik', '2'), (NULL, '100-3378', 'Automatisierungstechnik', '3'), (NULL, '100-3367', 'Laboratorium - INFI', '1'), (NULL, '100-3365', 'Theorie - INFI', '1'), (NULL, '100-3387', 'Laboratorium - Robotik', '28');
 INSERT INTO `lessons` (`id`, `id_teachers`, `hours`, `group`, `id_subjects`) VALUES (NULL, '1', '20', '30', '2'), (NULL, '1', '20', '30', '3'), (NULL, '2', '10', '100', '5'), (NULL, '3', '20', '150', '4'), (NULL, '3', '5', '1', '1');
-
+INSERT INTO `lessons` (`id`, `id_teachers`, `hours`, `group`, `id_subjects`) VALUES (NULL, '1', '5', '6', '2'), (NULL, '2', '4', '8', '4'), (NULL, '2', '7', '1', '5'), (NULL, '2', '6', '12', '1'), (NULL, '1', '6', '21', '3'), (NULL, '1', '6', '23', '4'), (NULL, '3', '1', '23', '5'), (NULL, '3', '2', '12', '2'), (NULL, '3', '6', '84', '3'), (NULL, '3', '15', '212', '3');
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
