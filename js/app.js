f5fc91246391ed4d008dde8b8e177c3bc9267da2
Michi2806 <michi2806@gmail.com>
2018-01-10 13:50:22 +0100

    Changes in Generated Stuff

--- js/app.js
+++ js/app.js
@@ -35,10 +35,10 @@ function addTeacher() {
 	};
 }
 
-function addLesson() {
+function addLesson(subjectId) {
 	var lessonRequest = new XMLHttpRequest();
 	lessonRequest.open('Post', './php/addLesson.php');
-	lessonRequest.send('{"teacher":"1","subject":"1","hours":"20", "group":"gruppe"}');
+	lessonRequest.send('{"teacher":"1","subject":"'+subjectId+'","hours":"20", "group":"gruppe"}');
 	lessonRequest.onreadystatechange = function() {
 		if (lessonRequest.readyState === DONE) {
 			if (lessonRequest.status === OK) {
@@ -111,12 +111,13 @@ dataRequest.onreadystatechange = function() {
 };
 
 function appendSubject(element, subject) {
-	element.insertAdjacentHTML('beforeend', '<div class="subject"><div class="info"><button type="button" name="button" class="closeButton">x</button><div class="half"><b>Subject: </b><span class="name">' + subject.title + '</span></div><div class="half"><b>Criteria: </b><span class="timecriteria"> 5 </span></div></div><div class="lessonContainer"><div class="lesson" id="subject' + subject.id + '"></div><button type="button" name="button" onclick="addLesson();"> + </button></div></div>');
+	element.insertAdjacentHTML('beforeend', '<div class="subject"><div class="info"><button type="button" name="button" class="closeButton">x</button><div class="half"><b>Subject: </b><span class="name">' + subject.title + '</span></div><div class="half"><b>Criteria: </b><span class="timecriteria"> 5 </span></div></div><div class="lessonContainer" id="subject' + subject.id + '"></div></div>');
 	for (var i = 0; i < subject.lessons.length; i++) {
 		appendLesson(document.getElementById('subject' + subject.id), subject.lessons[i]);
 	}
+	document.getElementById('subject' + subject.id).insertAdjacentHTML('beforeend', '<button type="button" name="button" onclick="addLesson('+subject.id+');"> + </button>');
 }
 
 function appendLesson(element, lesson) {
-	element.insertAdjacentHTML('beforeend', '<div class="lesson centered"><div class="row teacherrow"><span class="teacher">' + lesson.short + '</span></div><div class="row"><span class="half">Group </span><span class="half">' + lesson.group + '</span</div></div>');
+	element.insertAdjacentHTML('beforeend', '<div class="lessons"><div class="lesson centered"><div class="row teacherrow"><span class="teacher">' + lesson.short + '</span></div><div class="row"><span class="half">Group </span><span class="half">' + lesson.group + '</span</div></div></div>');
 }
